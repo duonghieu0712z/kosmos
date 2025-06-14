d73ef9566cc9c8055b4b5a4ed45d94bc4624c01c
Daisy <duonghieu0712z@gmail.com>
2025-06-15 00:24:41 +0700

    update: add loading splash

--- src/app/Splash.tsx
+++ src/app/Splash.tsx
@@ -2,12 +2,18 @@ import logo from '@/assets/images/logo.png';
 
 export default function Splash() {
     return (
-        <div className='flex h-full flex-col items-center justify-center'>
-            <img src={logo} alt='logo' className='size-42 rounded-4xl bg-zinc-200' />
-            <div className='mt-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text font-[calligraphy] text-4xl font-bold text-transparent'>
+        <div className='flex h-full flex-col items-center justify-center bg-gradient-to-br from-pink-100 via-purple-100 to-indigo-100'>
+            <img src={logo} alt='logo' className='size-42 rounded-4xl' />
+
+            <div className='bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 bg-clip-text font-[calligraphy] text-4xl font-bold text-transparent'>
                 {__APP_NAME__}
             </div>
-            <div className='text-[10px]'>v{__APP_VERSION__}</div>
+
+            <div className='relative flex size-24 items-center justify-center perspective-midrange'>
+                <div className='animate-rotate1 absolute size-full rounded-full border-b-4 border-[#5c5edc]' />
+                <div className='animate-rotate2 absolute size-full rounded-full border-r-4 border-[#9147ff]' />
+                <div className='animate-rotate3 absolute size-full rounded-full border-t-4 border-[#3b82f6]' />
+            </div>
         </div>
     );
 }
