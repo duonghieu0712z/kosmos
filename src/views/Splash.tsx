88bfbc08e16bec87c8dd71ec1b9493ba87521c7f
Daisy <duonghieu0712z@gmail.com>
2025-06-14 19:32:36 +0700

    update: remove unused commands

--- src/views/Splash.tsx
+++ src/views/Splash.tsx
@@ -4,7 +4,9 @@ export default function Main() {
     return (
         <div className='flex h-full flex-col items-center justify-center'>
             <img src={logo} alt='logo' className='size-42 rounded-4xl bg-zinc-200' />
-            <div className='mt-2 font-[calligraphy] text-4xl font-bold'>{__APP_NAME__}</div>
+            <div className='mt-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text font-[calligraphy] text-4xl font-bold text-transparent'>
+                {__APP_NAME__}
+            </div>
             <div className='text-[10px]'>v{__APP_VERSION__}</div>
         </div>
     );
