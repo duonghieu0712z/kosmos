2c2978628d9ac43428c8a395fdad65957cca021c
Daisy <duonghieu0712z@gmail.com>
2025-06-18 23:26:52 +0700

    update sidebar icon

--- src/app/dashboard/Sidebar.tsx
+++ src/app/dashboard/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { BookA, Eye, Globe2, Home, Hourglass, Map, PencilRuler, Settings } from 'lucide-react';
+import { BookText, Eye, Globe2, Home, Hourglass, Map, PencilRuler, Settings } from 'lucide-react';
 import { useState } from 'react';
 
 import logo from '@/assets/images/logo.png';
@@ -9,7 +9,7 @@ import SidebarSeparator from './SidebarSeparator';
 
 const menus = [
     { name: 'Home', icon: Home, to: 'home' },
-    { name: 'Story', icon: BookA, to: 'story' },
+    { name: 'Story', icon: BookText, to: 'story' },
     { name: 'Wiki', icon: PencilRuler, to: 'wiki' },
     { name: 'Map', icon: Map, to: 'map' },
     { name: 'Timeline', icon: Hourglass, to: 'timeline' },
@@ -20,18 +20,15 @@ const menus = [
 export default function Sidebar() {
     const [isExpanded, setIsExpanded] = useState(true);
     return (
-        <aside className={cn('bg-base-200 flex w-50 flex-col p-1', !isExpanded && 'w-[42px]')}>
+        <aside className={cn('bg-base-200 flex flex-col p-1')}>
             <header>
                 <button
-                    className={cn(
-                        'btn btn-ghost flex w-full justify-start gap-2 px-1 font-medium',
-                        !isExpanded && 'tooltip tooltip-right'
-                    )}
+                    className='btn btn-ghost flex w-full justify-start gap-2 px-1 font-medium'
                     onClick={() => setIsExpanded(!isExpanded)}
                 >
                     <img src={logo} alt='logo' className='size-6' />
                     {isExpanded && (
-                        <div className='bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 bg-clip-text font-[calligraphy] text-3xl font-bold text-transparent'>
+                        <div className='mr-2 bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 bg-clip-text font-[calligraphy] text-3xl font-bold text-transparent'>
                             {__APP_NAME__}
                         </div>
                     )}
