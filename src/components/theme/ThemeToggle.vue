6805938d2afcd1e3c679c552d0dd991a0763db98
duonghieu <duonghieu0712z@gmail.com>
2025-11-21 12:28:01 +0700

    refactor: update theme toggle

--- src/components/theme/ThemeToggle.vue
+++ src/components/theme/ThemeToggle.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { useColorMode } from '@vueuse/core';
-import { Moon, Sun } from 'lucide-vue-next';
+import { UseColorMode } from '@vueuse/components';
+import { Laptop2, Moon, Sun } from 'lucide-vue-next';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -9,28 +9,25 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-
-const mode = useColorMode();
 </script>
 
 <template>
-    <DropdownMenu>
-        <DropdownMenuTrigger as-child>
-            <Button class="hover:[&>svg]:stroke-sidebar-foreground size-12" size="icon" variant="ghost">
-                <Moon
-                    class="stroke-muted-foreground size-6 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
-                />
-                <Sun
-                    class="stroke-muted-foreground absolute size-6 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
-                />
-                <span class="sr-only">Toggle theme</span>
-            </Button>
-        </DropdownMenuTrigger>
+    <UseColorMode v-slot="color">
+        <DropdownMenu>
+            <DropdownMenuTrigger as-child>
+                <Button class="hover:[&>svg]:stroke-sidebar-foreground size-12" size="icon" variant="ghost">
+                    <Sun v-if="color.mode === 'light'" class="stroke-muted-foreground size-6" />
+                    <Moon v-else-if="color.mode === 'dark'" class="stroke-muted-foreground size-6" />
+                    <Laptop2 v-else class="stroke-muted-foreground size-6" />
+                    <span class="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenuTrigger>
 
-        <DropdownMenuContent align="end" side="right">
-            <DropdownMenuItem @click="mode = 'light'">Light</DropdownMenuItem>
-            <DropdownMenuItem @click="mode = 'dark'">Dark</DropdownMenuItem>
-            <DropdownMenuItem @click="mode = 'auto'">System</DropdownMenuItem>
-        </DropdownMenuContent>
-    </DropdownMenu>
+            <DropdownMenuContent align="end" side="right">
+                <DropdownMenuItem @click="color.mode = 'light'">Light</DropdownMenuItem>
+                <DropdownMenuItem @click="color.mode = 'dark'">Dark</DropdownMenuItem>
+                <DropdownMenuItem @click="color.mode = 'auto'">System</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    </UseColorMode>
 </template>
