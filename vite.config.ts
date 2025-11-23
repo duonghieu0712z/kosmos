de604e697eb607c54864e8506d142ccf0366130a
Daisy <duonghieu0712z@gmail.com>
2025-11-23 11:49:00 +0700

    feat: add vue devtools

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-    plugins: [vue(), tailwindcss()],
+    plugins: [vue(), tailwindcss(), vueDevTools()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
