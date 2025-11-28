2066c2db252cd7f940a26d68c15f3f813a2dc86d
Daisy <duonghieu0712z@gmail.com>
2025-11-28 23:32:47 +0700

    update tab height

--- src/modules/dashboard/MainContent.vue
+++ src/modules/dashboard/MainContent.vue
@@ -13,18 +13,18 @@ const { tabs, currentTab, activeTab, popTab } = useTabs();
 <template>
     <SidebarInset class="min-w-0 items-center justify-center">
         <Tabs class="size-full gap-0" :model-value="currentTab" @update:model-value="activeTab($event as string)">
-            <ScrollArea v-wheel-x orientation="horizontal">
-                <TabsList class="h-6 w-full justify-start rounded-none p-0">
+            <ScrollArea v-wheel-x class="h-8" orientation="horizontal">
+                <TabsList class="h-8 w-full justify-start rounded-none p-0">
                     <TabsTrigger
                         v-for="tab in tabs"
                         :key="tab.id"
-                        class="data-active:after:border-ring relative flex-none justify-end rounded-none pr-0.5 hover:cursor-pointer data-active:after:absolute data-active:after:bottom-0 data-active:after:left-0 data-active:after:w-full data-active:after:border-b-2 hover:[&>span]:opacity-100 data-active:[&>span]:opacity-100"
+                        class="data-active:after:border-ring relative h-8 min-w-30 flex-none justify-end rounded-none pr-1 hover:cursor-pointer data-active:after:absolute data-active:after:bottom-0 data-active:after:left-0 data-active:after:w-full data-active:after:border-b-2 hover:[&>span]:opacity-100 data-active:[&>span]:opacity-100"
                         :data-tab-id="`kosmos-tab-${tab.id}`"
                         :value="tab.id"
                         @click="activeTab(tab.id)"
                     >
                         <component :is="tab.icon" v-if="tab.icon" />
-                        <div class="flex-1 truncate text-xs">{{ tab.name }}</div>
+                        <div class="flex-1 truncate text-left text-xs">{{ tab.name }}</div>
                         <span
                             class="hover:bg-ring/20 rounded opacity-0"
                             @click="popTab(tab.id)"
@@ -34,8 +34,9 @@ const { tabs, currentTab, activeTab, popTab } = useTabs();
                                     ev.stopPropagation();
                                 }
                             "
-                            ><X
-                        /></span>
+                        >
+                            <X />
+                        </span>
                     </TabsTrigger>
                 </TabsList>
             </ScrollArea>
